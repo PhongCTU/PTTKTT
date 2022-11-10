2349527b5a65488dc1559ce94060d24d72ca7535
Phạm Thanh Phong <101854413+PhongCTU@users.noreply.github.com>
2022-11-10 08:17:34 +0700

    Update balo3.c

--- Thaman/balo3.c
+++ Thaman/balo3.c
@@ -5,17 +5,16 @@
 // cau truc mot do vat
 typedef struct
 {
-    char TenDV[20];
-    float TL, GT, DG; // Trong luong, Gia tri, Don gia
+    char TenDV[20];// ten do vat co do dai 20 ky tu
+    float TL, GT, DG; // Trong luong, Gia tri, Don gia cua mot do vat
     int Phuong_An;    // So phuong an duoc chon
 } DoVat;
 
-DoVat *Read_File(char file_name[], float *W, int *n)
+DoVat *Read_File( float *W, int *n) // ba lo co trong luong w, so luong do vat n
 {
-    FILE *f;
-    f = fopen(file_name, "r");                    // mo file de doc
+    FILE *f = fopen("balo3.txt", "r");                    // mo file de doc
     fscanf(f, "%f", W);                           // Xac dinh trong luong Ba lo
-    DoVat *dsdv = (DoVat *)malloc(sizeof(DoVat)); // khoi tao mot danh sach do vat
+    DoVat *dsdv = (DoVat *)malloc(sizeof(DoVat)); // ca p phat mot danh sach dong do vat
     int i = 0;
     while (!feof(f)) // khi con tro con trong file
     {
@@ -54,7 +53,7 @@ void in_dsdv(DoVat *dsdv, int n, float W)
 {
     int i;
     float TongTL = 0.0, TongGT = 0.0; // khoi ta tong gia tri va tong trong luong = 0
-    printf("\nPhuong an Cai Ba lo 1 dung thuat toan THAM AN nhu sau:\n");
+    printf("\nPhuong an Cai Ba lo 3 dung thuat toan THAM AN nhu sau:\n");
     printf("\nTrong luong cua ba lo = %-9.2f\n", W);
     printf("|---|--------------------|---------|---------|---------|-----------|\n");
     printf("|STT|     Ten Do Vat     | T Luong | Gia Tri | Don Gia | Phuong an |\n");
@@ -63,27 +62,27 @@ void in_dsdv(DoVat *dsdv, int n, float W)
     {
         printf("|%2d |%-20s|%9.2f|%9.2f|%9.2f|%6d     |\n",
                i + 1, dsdv[i].TenDV, dsdv[i].TL, dsdv[i].GT, dsdv[i].DG, dsdv[i].Phuong_An);
-        TongTL = TongTL + dsdv[i].Phuong_An * dsdv[i].TL;
-        TongGT = TongGT + dsdv[i].Phuong_An * dsdv[i].GT;
+        TongTL = TongTL + dsdv[i].Phuong_An * dsdv[i].TL; // TTL = TTL + so Phuong an * Trong luong cua vat
+        TongGT = TongGT + dsdv[i].Phuong_An * dsdv[i].GT;// TGT = TGT + so phuong an * Gia tri cua vat
     }
     printf("|---|--------------------|---------|---------|---------|-----------|\n");
     printf("Phuong an (theo thu tu DG giam dan): X(");
     for (i = 0; i < n - 1; i++)
     {
-        printf("%d,", dsdv[i].Phuong_An);
+        printf("%d,", dsdv[i].Phuong_An); // in tu puong an du tien den truoc phuong an cuoi cung
     }
-    printf("%d)", dsdv[n - 1].Phuong_An);
+    printf("%d)", dsdv[n - 1].Phuong_An); // in phuong an cuoi cung
     printf("\nTong trong luong = %-9.2f\n", TongTL);
     printf("Tong gia tri = %-9.2f\n", TongGT);
 }
 void greedy(DoVat *dsdv, int n, float W)
 {
-    for (int i = 0; i < n; i++)
+	int i;
+    for ( i = 0; i < n; i++)
     {
-        dsdv[i].Phuong_An = (W / dsdv[i].TL); // phuong an = phan nguyen cua trong luong cua balo / trong luong do vat
-        if (dsdv[i].Phuong_An > 1)
-            dsdv[i].Phuong_An = 1;
-        W = W - dsdv[i].Phuong_An * dsdv[i].TL; // trong luong con lai cua balo = phuong an * trong luong cua vat.
+        dsdv[i].Phuong_An = (W / dsdv[i].TL);   // phuong an = phan nguyen cua trong luong cua balo / trong luong do vat
+        if(dsdv[i].Phuong_An > 1) dsdv[i].Phuong_An = 1; // neu so phuong an lon hon 1 thi phuong an bang 1
+		W = W - dsdv[i].Phuong_An * dsdv[i].TL; // trong luong con lai cua balo = phuong an * trong luong cua vat.
     }
 }
 int main()
@@ -91,7 +90,7 @@ int main()
     int n;
     float W;
     DoVat *dsdv;
-    dsdv = Read_File("D:/PTTKTT/Thaman/balo2.txt", &W, &n);
+    dsdv = Read_File(&W, &n);
     BubbleSort(dsdv, n);
     greedy(dsdv, n, W);
     in_dsdv(dsdv, n, W);
